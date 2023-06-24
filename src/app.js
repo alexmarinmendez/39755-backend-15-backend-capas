6656d5cbafd917535d5098de9f525effc948f8fd
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-06-24 13:03:19 -0500

    add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import userRouter from './routers/user.router.js'
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 8080
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 app.use('/api/users', userRouter)
 
 app.listen(PORT, () => console.log(`Server up on port=${PORT}`))
